e9a3cda48f2426ccfe60e7315add5af444b6e295
nadineboukadida <78963245+nadineboukadida@users.noreply.github.com>
2023-12-08 01:21:34 +0100

    Added - sentry

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as Sentry from "@sentry/node";
+import { ProfilingIntegration } from "@sentry/profiling-node";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  Sentry.init({
+    dsn: 'https://85f098cc9884f0052b9eaf749af48bda@o4506357247246336.ingest.sentry.io/4506357251899392',
+    integrations: [
+      new ProfilingIntegration(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0,
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 1.0,
+  });
   await app.listen(3000);
 }
 bootstrap();
